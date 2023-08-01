3dafe7de43573cd573669f85a569925dd0ac0f1a
K1kva2006 <115255875+K1kva2006@users.noreply.github.com>
2023-08-01 11:25:44 +0400

    v1.0

---
+++ app.js
@@ -0,0 +1,22 @@
+const year = document.getElementById("year");
+const month = document.getElementById("month");
+const day = document.getElementById("day");
+
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+
+
+function dataTime() {
+    year.textContent = new Date().getFullYear() + " / ";
+    month.textContent = new Date().getMonth() + " / ";
+    day.textContent = new Date().getDate();
+
+    hours.textContent = new Date().getHours() + " : ";
+    minutes.textContent = new Date().getMinutes() + " : ";
+    seconds.textContent = new Date().getSeconds();
+
+    setInterval(dataTime, 1000)
+}
+window.addEventListener("load", dataTime)
+
